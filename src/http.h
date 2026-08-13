99e5a2ee2228507f2ab2711bff0d3be1e5e619e5
arhayashi <tuhayashi00@gmail.com>
2026-08-13 17:43:45 -0400

    Update http.h

--- src/http.h
+++ src/http.h
@@ -2,8 +2,9 @@
 
 #define HTTP_H
 
+void create_http_response(char *, char *);
 void handle_http_request(int);
-void handle_http_response(int, char *, char *);
+void handle_http_response(int, char *, char *, int);
 int parse_http_request(char *, char *, char *);
 
 #endif
