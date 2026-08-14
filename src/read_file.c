d6c7823f026c647495c10b447fd807d2ed608159
arhayashi <tuhayashi00@gmail.com>
2026-08-14 13:27:31 -0400

    Create struct to hold file content

---
+++ src/read_file.c
@@ -0,0 +1,29 @@
+/*
+ * This file contains code to gather information about a file and put its
+ * content in a file_content_t struct.
+ */
+
+#include <stdlib.h>
+
+#include "read_file.h"
+
+/*
+ * Dynamically allocates a file_content_t struct.
+ */
+
+file_cont_t *create_file_cont_t() {
+    file_cont_t *file_cont = malloc(sizeof(file_cont_t));
+
+    if (file_cont == NULL) {
+        return NULL;
+    }
+
+    file_cont->size = 0;
+    file_cont->content = NULL;
+
+    return file_cont;
+} /* create_file_cont_t() */
+
+int main(void) {
+
+}
