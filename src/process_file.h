19cd2970a26c423bddabe3a90004c938bbd48b44
arhayashi <tuhayashi00@gmail.com>
2026-08-15 18:06:20 -0400

    Add forward declarations

--- src/process_file.h
+++ src/process_file.h
@@ -7,4 +7,9 @@ typedef struct {
     void *content;
 } file_cont_t;
 
+void close_file(FILE **);
+file_cont_t *create_file_cont_t();
+void free_file_cont_t(file_cont_t **);
+file_cont_t *read_file_cont(char *);
+
 #endif
