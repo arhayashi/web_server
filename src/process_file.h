97be350f7e80cf04ec0c5dc0e2aa5282c8cb9a60
arhayashi <tuhayashi00@gmail.com>
2026-08-17 16:23:06 -0400

    Update forward declaration

--- src/process_file.h
+++ src/process_file.h
@@ -2,13 +2,17 @@
 
 #define READ_FILE_H
 
+#define FILE_SUCC (1)  /* indicates file read and stored successfully */
+#define FILE_NEXS (0)  /* indicates file doesn't exist or invalid perms */
+#define FILE_ERR (-1)  /* indicates file reading or storing failed */
+
 typedef struct {
     int size;
     void *content;
 } file_cont_t;
 
 void close_file(FILE **);
-file_cont_t *create_file_cont_t();
+file_cont_t *create_file_cont_t(int, void *);
 void free_file_cont_t(file_cont_t **);
 file_cont_t *read_file_cont(char *);
 
