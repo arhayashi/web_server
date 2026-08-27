621b19117606a392d803f4100aa7d2eb7c675729
arhayashi <tuhayashi00@gmail.com>
2026-08-26 22:44:41 -0400

    Add error macro for when acccessing directory

--- src/process_file.h
+++ src/process_file.h
@@ -5,6 +5,7 @@
 #define FILE_SUCC (1)  /* indicates file read and stored successfully */
 #define FILE_NEXS (0)  /* indicates file doesn't exist or invalid perms */
 #define FILE_ERR (-1)  /* indicates file reading or storing failed */
+#define FILE_DIR (-2)  /* indicates file is actually a directory */
 
 typedef struct {
     int size;
@@ -14,6 +15,6 @@ typedef struct {
 void close_file(FILE **);
 file_cont_t *create_file_cont_t(int, void *);
 void free_file_cont_t(file_cont_t **);
-file_cont_t *read_file_cont(char *);
+file_cont_t *read_file_cont(char *, int *);
 
 #endif
